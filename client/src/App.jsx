18246a4f1882b7e43d6f35c78086a10496e86eb5
Muhammad Zunoon Ali <zunnoon2006@gmail.com>
2025-08-03 13:10:26 +0500

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Header from '@/components/Header'
 import About from '@/components/About'
 import Skill from '@/components/Skills'
 import Projects from '@/components/Projects'
