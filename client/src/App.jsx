125873fc9b3b2506e6587c9f03fbfbfeceaade32
Muhammad Zunoon Ali <zunnoon2006@gmail.com>
2025-08-03 12:56:28 +0500

    Deploy: build working, ready for Netlify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Skill from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Education from'./components/Education'
-import Goal from './components/Goal'
-import ScrollArrow from './components/ScrollArrow'
+import React from 'react';
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Skill from '@/components/Skills'
+import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+import Education from '@/components/Education'
+import Goal from '@/components/Goal'
+import ScrollArrow from '@/components/ScrollArrow'
  
 
 function App() {
